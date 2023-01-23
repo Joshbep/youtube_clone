9f21d92dfd1697baf478c6bea89fc98d41875ec8
jbep <joshuabemartin@gmail.com>
2023-01-23 18:02:48 -0500

    started stlying stack from mui a little

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-import { logo } from '../utils/constants'
+import { logo } from '../utils/constants';
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack 
+    direction="row" 
+    alignItems="center" 
+    p={2} 
+    sx={{position: 'sticky', background: '#000', top:0, justifyContent: 'space-between'}}>
     </Stack>
   )
 }
