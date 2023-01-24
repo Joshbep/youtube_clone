9ddedeefb2310affd752bb483fd26190bdf39949
jbep <joshuabemartin@gmail.com>
2023-01-24 15:45:03 -0500

    initial search bar styling done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { React , useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
-import { SearchIcon } from '@mui/icons-material'
+import { Search } from '@mui/icons-material'
 
 const SearchBar = () => {
   return (
@@ -16,7 +16,21 @@ const SearchBar = () => {
             mr: { sm: 5 }
         }}
     >
-        SearchBar
+        <input 
+        className="search-bar"
+        placeholder="Search"
+        value=""
+        onChange={() => {}}
+        />
+        <IconButton
+            type="submit"
+            sx={{
+                p:'10px',
+                color: 'red'
+            }}
+        >
+            <Search />
+        </IconButton>
     </Paper>
   )
 }
