c1cc447145f79c7885b8677fdcf0b7cdbd83dd4c
jbep <joshuabemartin@gmail.com>
2023-01-24 16:00:51 -0500

    started on feed and sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { React, useState, useEffect} from 'react'
+import { Box, Stack, Typography } from '@mui/material';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{
+        flexDirection: { sx:
+          'column', md: "row"
+        }
+      }}
+    >
+      <Box
+      sx={{
+        height: {sx: 'auto', md:'92vh'},
+        borderRight: '1px solid #7d3d3d',
+        px: { sx: 0, md: 2}
+      }}
+      >
+        {/* <SideBar /> */}
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff'}}
+        >
+          Copyright 2023 Josh Pasour Youtube Clone
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
