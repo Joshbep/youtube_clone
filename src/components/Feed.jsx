ce9f83193f7266124fcf11c1c7b1175000f2d4fa
jbep <joshuabemartin@gmail.com>
2023-01-25 15:33:53 -0500

    started working on sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,6 @@
 import { React, useState, useEffect} from 'react'
 import { Box, Stack, Typography } from '@mui/material';
+import SideBar from './SideBar'
 
 const Feed = () => {
   return (
@@ -17,7 +18,7 @@ const Feed = () => {
         px: { sx: 0, md: 2}
       }}
       >
-        {/* <SideBar /> */}
+        <SideBar />
         <Typography
           className="copyright"
           variant="body2"
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Stack } from '@mui/material';
+import { Categories } from '../utils/constants';
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <Stack
+      direction='row',
+      sx={{
+        overflowY: 'auto',
+        height: { sx: 'auto', md: '95%'},
+        flexDirection: { md: 'column'},
+      }}
+    >
+
+    </Stack>
   )
 }
 
