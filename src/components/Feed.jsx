2d25200369f0f5514664b9258efcee7b2dcabce5
jbep <joshuabemartin@gmail.com>
2023-01-25 15:57:36 -0500

    added elements to the feed and styled them

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -27,6 +27,34 @@ const Feed = () => {
           Copyright 2023 Josh Pasour Youtube Clone
         </Typography>
       </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: 'auto',
+          height: '90vh',
+          flex: 2
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: 'white'
+          }}
+        >
+          New
+          <span
+          style={{
+            color: '#fc1503'
+          }}
+          >
+            videos
+          </span>
+        </Typography>
+
+        <Videos />
+      </Box>
     </Stack>
   )
 }
