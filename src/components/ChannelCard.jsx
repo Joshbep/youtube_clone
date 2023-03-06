a4946083c736bfd54d1729afefdbc75c2ec3413e
jbep <joshuabemartin@gmail.com>
2023-03-06 18:31:27 -0500

    started working on video and channel card

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { Box, CardContent, CardMedia, Typography} from '@mui/material';
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const ChannelCard = () => {
   return (
