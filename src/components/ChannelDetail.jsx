eef88e88ab8d4f8528592024a10cd340dab785ab
jbep <joshuabemartin@gmail.com>
2023-03-06 19:06:14 -0500

    starting to build out channel detail page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState();
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channe;s?part="snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+  }, [id]);
+
   return (
-    <div>ChannelDetail</div>
+    <Box>
+    </Box>
   )
 }
 
